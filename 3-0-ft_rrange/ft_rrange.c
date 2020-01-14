560069fa7d2318d5237ab58be03d636e8524576a
Xing Tang <xtang@c2r1p3.hive.fi>
2020-01-14 18:07:44 +0200

    20Jan14

--- 3-0-ft_rrange/ft_rrange.c
+++ 3-0-ft_rrange/ft_rrange.c
@@ -1,55 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rrange.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xtang <xtang@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/14 17:19:54 by xtang             #+#    #+#             */
+/*   Updated: 2020/01/14 17:29:43 by xtang            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <stdlib.h>
 
-unsigned int ft_int_abs(int start, int end)
+unsigned int	ft_int_abs(int start, int end)
 {
-    unsigned int range;
-    range = (start >= end) ? (start - end) : (end - start);
-    return (range);
- }
+	unsigned int range;
 
-
-int *ft_rrange(int start, int end)
-{
-    int i;
-    int *ptr;
-
-    i = 0;
-    unsigned int range;
-
-    range = ft_int_abs(start, end);
-    ptr = (int *)malloc(sizeof(int) * (range + 1));
-    while ( start != end )
-    {
-        ptr[i] = end;
-        if ( start >= end)
-            end++;
-        else
-            end--;
-        i++;
-    }
-    ptr[i] = end;
-    return (ptr);
+	range = (start >= end) ? (start - end) : (end - start);
+	return (range);
 }
 
-int main (void)
+int				*ft_rrange(int start, int end)
 {
-    int str;
-    int end;
-    int *ptr;
-    unsigned int i;
-    unsigned int len;
+	int				i;
+	int				*ptr;
+	unsigned int	range;
 
-    str = 0;
-    end = -3;
-    len = ft_int_abs(str,end) + 1;
+	i = 0;
+	range = ft_int_abs(start, end);
+	ptr = (int *)malloc(sizeof(int) * (range + 1));
+	while (start != end)
+	{
+		ptr[i] = end;
+		if (start >= end)
+			end++;
+		else
+			end--;
+		i++;
+	}
+	ptr[i] = end;
+	return (ptr);
+}
 
-    ptr = ft_rrange(str, end);
-    i = 0; 
-    while(i < len )
-    {
-        printf("%d, ", ptr[i]);
-        i++;
-    }
+int				main(void)
+{
+	int				str;
+	int				end;
+	int				*ptr;
+	unsigned int	i;
+	unsigned int	len;
 
+	str = 0;
+	end = -3;
+	len = ft_int_abs(str, end) + 1;
+	ptr = ft_rrange(str, end);
+	i = 0;
+	while (i < len)
+	{
+		printf("%d, ", ptr[i]);
+		i++;
+	}
 }
--- 3-0-hidenp/hidenp.c
+++ 3-0-hidenp/hidenp.c
@@ -1,24 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hidenp.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xtang <xtang@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/14 17:34:03 by xtang             #+#    #+#             */
+/*   Updated: 2020/01/14 17:38:03 by xtang            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include <stdio.h>
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	int i;
 	int j;
-	
+
 	if (argc == 3)
 	{
 		i = 0;
 		j = 0;
-		while(argv[1][i] != '\0')
-		{	
-			while(argv[2][j] != '\0')
+		while (argv[1][i] != '\0')
+		{
+			while (argv[2][j] != '\0')
 			{
-				if ( argv[1][i] == argv[2][j])
+				if (argv[1][i] == argv[2][j])
 				{
 					j++;
-					i++;					
-					break ; 	
+					i++;
+					break ;
 				}
 				j++;
 				if (argv[2][j] == '\0')
@@ -32,10 +44,8 @@ int main(int argc, char **argv)
 				write(1, "1\n", 2);
 				return (0);
 			}
-		}	
+		}
 	}
-	write (1, "\n", 1);
+	write(1, "\n", 1);
 	return (0);
-
 }
-
