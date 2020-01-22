376aaae52a6763b3b03f5f1b7b964f0c07f8ce41
tangxing1220 <tangxing1220@outlook.com>
2020-01-23 01:53:04 +0200

    at home 20Jan22

--- 3-4-str_capitalizer/str_capitalizer_v2.c
+++ 3-4-str_capitalizer/str_capitalizer_v2.c
@@ -30,21 +30,30 @@ int	main(int argc, char **argv)
 		flag = 0;
 		while (argv[i][j] != '\0')
 		{
-			if (argv[i][j] == ' ' || argv[i][j] == '\t')
-				flag = 0;
-			else if (argv[i][j] >= 'a' && argv[i][j] <= 'z')
+			if (flag == 0)
 			{
-				if (flag == 0)
+				if (argv[i][j] == ' ' || argv[i][j] == '\t')
+					flag = 0;
+				else if(argv[i][j] >= 'a' && argv[i][j] <= 'z')
+				{
 					argv[i][j] = argv[i][j] - 'a' + 'A';
-				flag = 1;
+					flag = 1;
+				}
+				else
+					flag = 1;
 			}
-			else if (argv[i][j] >= 'A' && argv[i][j] <= 'Z')
-			{
-				if (flag == 1)
+			else if (flag == 1)
+			{       
+				if (argv[i][j] == ' ' || argv[i][j] == '\t')
+					flag = 0;
+				else if(argv[i][j] >= 'A' && argv[i][j] <= 'Z')
+				{
 					argv[i][j] = argv[i][j] - 'A' + 'a';
+					flag = 1;
+				}
+				else
+					flag = 1;
 			}
-			else
-				flag = 1;
 			write(1, &argv[i][j], 1);
 			j++;
 		}
