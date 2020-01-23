d2306d6a06083e34847f8956ac9fa42e6d011566
Xing Tang <xtang@c2r1p3.hive.fi>
2020-01-23 12:02:25 +0200

    20Jan23

--- 4-0-fprime/fprime.c
+++ 4-0-fprime/fprime.c
@@ -1,34 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fprime.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xtang <xtang@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/23 11:53:57 by xtang             #+#    #+#             */
+/*   Updated: 2020/01/23 11:56:55 by xtang            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    unsigned int i;
-    unsigned int nbr;
+	unsigned int i;
+	unsigned int nbr;
 
-    if (argc == 2)
-    {
-       nbr = atoi(argv[1]);
-       if (nbr == 1)
-            {
-                printf("%d\n", nbr);
-                return (0);
-            }
-        i = 2;
-        while (i <= nbr )
-        {
-                if (nbr % i == 0)
-                {
-                    nbr = nbr / i;
-                    printf("%d", i);
-                    if (nbr != 1)
-                        printf("*");
-                }
-                else
-                    i++;            
-        }
-    }
-    printf("\n");
-    return (0);
-
+	if (argc == 2)
+	{
+		nbr = atoi(argv[1]);
+		if (nbr == 1)
+		{
+			printf("%d\n", nbr);
+			return (0);
+		}
+		i = 2;
+		while (i <= nbr)
+		{
+			if (nbr % i == 0)
+			{
+				nbr = nbr / i;
+				printf("%d", i);
+				if (nbr != 1)
+					printf("*");
+			}
+			else
+				i++;
+		}
+	}
+	printf("\n");
+	return (0);
 }
